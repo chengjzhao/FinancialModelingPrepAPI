a6654294bad55dcb731104c9288fd94315468d2d
Henry Zhao <chengjzhao@gmail.com>
2019-09-20 23:28:05 -0500

    chore: add prettier config files

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  arrowParens: "always"
+};
