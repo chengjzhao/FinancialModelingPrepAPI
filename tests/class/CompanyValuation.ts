d3d636131541e2b477c7984350c8ee6f406ae222
Henry Zhao <chengjzhao@gmail.com>
2019-09-21 03:16:14 -0500

    test: create test for financialStatements()

--- tests/class/CompanyValuation.ts
+++ tests/class/CompanyValuation.ts
@@ -1,10 +1,11 @@
 import 'mocha';
 import { expect } from 'chai';
 import CompanyValuation from '../../src/class/CompanyValuation';
-import { CompanyProfile } from '../../src/compiler/types';
+import { CompanyProfile, FinancialStatements } from '../../src/compiler/types';
 
 describe('CompanyValuation', () => {
   const companyValuation = new CompanyValuation();
+  const symbol = 'AAPL'; // Apple Inc.
 
   it('should return an instance of HTTP on initialization', () => {
     expect(companyValuation).to.be.an('object');
@@ -12,11 +13,26 @@ describe('CompanyValuation', () => {
   });
 
   describe('profile()', () => {
-    const symbol = 'AAPL'; // Apple Inc.
     let response: CompanyProfile;
 
     before(async () => {
       response = await companyValuation.profile(symbol);
+    });
+
+    it('should return a response as an object', () => {
+      expect(response).to.be.an('object');
+    });
+
+    it(`should have symbol property equals to ${symbol}`, () => {
+      expect(response.symbol).eq(symbol);
+    });
+  });
+
+  describe('financialStatements()', () => {
+    let response: FinancialStatements;
+
+    before(async () => {
+      response = await companyValuation.financialStatements(symbol);
     });
 
     it('should return a response as an object', () => {
