253c0382a1ac47bc6e0e88a8503b530e38fcd92d
Henry Zhao <chengjzhao@gmail.com>
2019-09-21 01:51:12 -0500

    chore: create types file

---
+++ src/compiler/types.ts
@@ -0,0 +1,21 @@
+export interface CompanyProfile {
+  symbol: string;
+  profile: {
+    price:number;
+    beta: string;
+    volAvg: string;
+    mktCap: string;
+    lastDiv: string;
+    range: string;
+    changes: number;
+    changesPercentage: string;
+    companyName: string;
+    exchange: string;
+    industry: string;
+    website: string;
+    description: string;
+    ceo: string;
+    sector: string;
+    image: string;
+  }
+}
