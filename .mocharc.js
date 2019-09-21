23eb9e9f5436dea4792a5dac089926b521d81744
Henry Zhao <chengjzhao@gmail.com>
2019-09-20 22:48:01 -0500

    test: add mocha test config file and script

---
+++ .mocharc.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * By default, Mocha will search for a config file if --config is not specified
+ * @see https://mochajs.org/#configuring-mocha-nodejs
+ *
+ * Mocha will search for — and use — only one. The priority is:
+ * .mocharc.js
+ * .mocharc.yaml
+ * .mocharc.yml
+ * .mocharc.jsonc
+ * .mocharc.json
+ */
+module.exports = {
+  diff: true,
+  extension: ['ts'],
+  opts: false,
+  package: './package.json',
+  reporter: 'spec',
+  slow: 75,
+  ui: 'bdd',
+  exit: true,
+  recursive: true,
+  timeout: 15000,
+  spec: ['./tests/**'],
+};
